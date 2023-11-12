ebaa5a0a7c539e64398a250e130b67c392e28dea
Admin <Admin@ADMIN>
2023-11-12 18:36:10 +0700

    xóa đăng ký

--- QL/QL/Form1.Designer.cs
+++ QL/QL/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -58,7 +57,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(159, 216);
+            this.button1.Location = new System.Drawing.Point(257, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 55);
             this.button1.TabIndex = 2;
@@ -68,7 +67,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(465, 216);
+            this.button2.Location = new System.Drawing.Point(436, 216);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 55);
             this.button2.TabIndex = 2;
@@ -93,22 +92,11 @@
             this.textBox2.Size = new System.Drawing.Size(236, 26);
             this.textBox2.TabIndex = 1;
             // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(317, 216);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(89, 55);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Đăng Ký";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(771, 449);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -130,7 +118,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
