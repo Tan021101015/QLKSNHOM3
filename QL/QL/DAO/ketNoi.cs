43274b551f160670b746b85b668069bbd34d577b
qthinh <thinhvta480@gmail.com>
2023-12-05 10:07:47 +0700

    qlhd

--- QL/QL/DAO/ketNoi.cs
+++ QL/QL/DAO/ketNoi.cs
@@ -15,7 +15,7 @@ namespace QL.DAO
         {
             try
             {
-                string sql = @"Data Source=M22\SQLEXPRESS;Initial Catalog=QLKSNHOM3;Integrated Security=True";
+                string sql = @"Data Source=M21\SQLEXPRESS;Initial Catalog=QLKSNHOM3;Integrated Security=True";
                 cnn.ConnectionString = sql;
                 if (cnn.State == ConnectionState.Closed)
                 {
